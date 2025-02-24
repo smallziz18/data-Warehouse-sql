049cf2a5b2b965c0280cef83a6b2224168e41bdf
abdoul aziz Diouf <133543436+smallziz18@users.noreply.github.com>
2025-02-24 13:05:08 +0000

    Update load_bronze.sql

--- scripts/bronze/load_bronze.sql
+++ scripts/bronze/load_bronze.sql
@@ -1,83 +1,108 @@
 -- ******************************************
 -- Script d'Insertion en Masse des Données CSV
 -- ******************************************
--- Objectif : Ce script permet de vider les tables existantes dans le schéma "bronze" 
---            et d'y insérer des données en masse à partir de fichiers CSV.
---
--- Les fichiers sources sont issus des répertoires :
--- - source_crm : contient des informations sur les clients, produits et ventes.
--- - source_erp : contient des informations sur les clients ERP, les localisations et les catégories produits.
---
--- Tables concernées :
--- 1. crm_customer_info
--- 2. crm_product_info
--- 3. crm_sales_details
--- 4. erp_customer_az12
--- 5. erp_loc_a101
--- 6. erp_px_cat_g1v2
---
--- Le processus est le suivant :
--- 1. Troncature (vidage) des tables existantes.
--- 2. Insertion des données depuis des fichiers CSV avec l'utilisation de BULK INSERT pour des performances optimales.
--- 3. Chaque fichier CSV est traité indépendamment et les en-têtes de chaque fichier sont ignorés (FIRSTROW = 2).
--- 4. La délimitation des colonnes dans les fichiers CSV est effectuée par des virgules (FIELDTERMINATOR = ',').
--- 5. La table est verrouillée (TABLOCK) pendant l'insertion en masse pour améliorer la performance.
+-- Objectif :
+-- Ce script permet de vider les tables existantes dans le schéma "bronze"
+-- et d'y insérer des données en masse à partir de fichiers CSV. Il inclut un suivi
+-- du temps d'exécution de chaque opération pour chaque table et donne un temps total
+-- d'exécution du script.
 
--- ******************************************
--- Début du script
 -- ******************************************
 
 -- Utilisation de la base de données DataWarehouse
 USE DataWarehouse;
 GO
 
--- ****************************
+-- ******************************************
 -- Création ou modification de la procédure stockée
--- ****************************
+-- ******************************************
 
 CREATE OR ALTER PROCEDURE bronze.load_bronze 
 AS
 BEGIN
+    -- Déclaration des variables de temps
+    DECLARE 
+        @starttime DATETIME,  -- Heure de début de chaque opération
+        @endtime DATETIME,    -- Heure de fin de chaque opération
+        @total_starttime DATETIME,  -- Heure de début du script
+        @total_endtime DATETIME;    -- Heure de fin du script
+
+    -- Heure de début du script
+    SET @total_starttime = GETDATE();
+    PRINT '======================================================================';
+    PRINT 'Début du script - ' + CAST(@total_starttime AS VARCHAR(19));
+    PRINT '======================================================================';
+
     BEGIN TRY
         -- Début de la transaction pour garantir que toutes les étapes sont cohérentes
         BEGIN TRANSACTION;
 
-        -- ****************************
+        -- ******************************************
         -- Section 1 : Troncature des tables (Clear des données existantes)
-        -- ****************************
+        -- ******************************************
 
-        PRINT 'Vider la table crm_customer_info';
+        -- Troncature de la table crm_customer_info
+        SET @starttime = GETDATE();
+        PRINT 'Vider la table crm_customer_info...';
         TRUNCATE TABLE bronze.crm_customer_info;
+        SET @endtime = GETDATE();
+        PRINT 'Temps d exécution pour crm_customer_info TRUNCATE : ' + CAST(DATEDIFF(SECOND, @starttime, @endtime) AS VARCHAR(10)) + ' secondes';
 
-        PRINT 'Vider la table crm_product_info';
+        -- Troncature de la table crm_product_info
+        SET @starttime = GETDATE();
+        PRINT 'Vider la table crm_product_info...';
         TRUNCATE TABLE bronze.crm_product_info;
+        SET @endtime = GETDATE();
+        PRINT 'Temps d exécution pour crm_product_info TRUNCATE : ' + CAST(DATEDIFF(SECOND, @starttime, @endtime) AS VARCHAR(10)) + ' secondes';
 
-        PRINT 'Vider la table crm_sales_details';
+        -- Troncature de la table crm_sales_details
+        SET @starttime = GETDATE();
+        PRINT 'Vider la table crm_sales_details...';
         TRUNCATE TABLE bronze.crm_sales_details;
+        SET @endtime = GETDATE();
+        PRINT 'Temps dexécution pour crm_sales_details TRUNCATE : ' + CAST(DATEDIFF(SECOND, @starttime, @endtime) AS VARCHAR(10)) + ' secondes';
 
-        PRINT 'Vider la table erp_customer_az12';
+        -- Troncature de la table erp_customer_az12
+        SET @starttime = GETDATE();
+        PRINT 'Vider la table erp_customer_az12...';
         TRUNCATE TABLE bronze.erp_customer_az12;
+        SET @endtime = GETDATE();
+        PRINT 'Temps d exécution pour erp_customer_az12 TRUNCATE : ' + CAST(DATEDIFF(SECOND, @starttime, @endtime) AS VARCHAR(10)) + ' secondes';
 
-        PRINT 'Vider la table erp_loc_a101';
+        -- Troncature de la table erp_loc_a101
+        SET @starttime = GETDATE();
+        PRINT 'Vider la table erp_loc_a101...';
         TRUNCATE TABLE bronze.erp_loc_a101;
+        SET @endtime = GETDATE();
+        PRINT 'Temps d exécution pour erp_loc_a101 TRUNCATE : ' + CAST(DATEDIFF(SECOND, @starttime, @endtime) AS VARCHAR(10)) + ' secondes';
 
-        PRINT 'Vider la table erp_px_cat_g1v2';
+        -- Troncature de la table erp_px_cat_g1v2
+        SET @starttime = GETDATE();
+        PRINT 'Vider la table erp_px_cat_g1v2...';
         TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+        SET @endtime = GETDATE();
+        PRINT 'Temps d exécution pour erp_px_cat_g1v2 TRUNCATE : ' + CAST(DATEDIFF(SECOND, @starttime, @endtime) AS VARCHAR(10)) + ' secondes';
 
-        -- ****************************
+        -- ******************************************
         -- Section 2 : Chargement des données depuis des fichiers CSV
-        -- ****************************
+        -- ******************************************
 
-        PRINT 'Insertion des données dans crm_customer_info depuis le fichier CSV';
+        -- Insertion dans crm_customer_info
+        SET @starttime = GETDATE();
+        PRINT 'Insertion des données dans crm_customer_info...';
         BULK INSERT bronze.crm_customer_info
         FROM 'D:\data-engeneering\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
         WITH (
-            FIRSTROW = 2,  -- Ignore la première ligne qui contient les noms de colonnes
-            FIELDTERMINATOR = ',',  -- Délimiteur de champ
-            TABLOCK  -- Verrouillage de la table pour améliorer les performances lors de l'insertion en masse
+            FIRSTROW = 2,
+            FIELDTERMINATOR = ',',
+            TABLOCK
         );
+        SET @endtime = GETDATE();
+        PRINT 'Temps d exécution pour crm_customer_info BULK INSERT : ' + CAST(DATEDIFF(SECOND, @starttime, @endtime) AS VARCHAR(10)) + ' secondes';
 
-        PRINT 'Insertion des données dans crm_product_info depuis le fichier CSV';
+        -- Insertion dans crm_product_info
+        SET @starttime = GETDATE();
+        PRINT 'Insertion des données dans crm_product_info...';
         BULK INSERT bronze.crm_product_info
         FROM 'D:\data-engeneering\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
         WITH (
@@ -85,8 +110,12 @@ BEGIN
             FIELDTERMINATOR = ',',
             TABLOCK
         );
+        SET @endtime = GETDATE();
+        PRINT 'Temps d exécution pour crm_product_info BULK INSERT : ' + CAST(DATEDIFF(SECOND, @starttime, @endtime) AS VARCHAR(10)) + ' secondes';
 
-        PRINT 'Insertion des données dans crm_sales_details depuis le fichier CSV';
+        -- Insertion dans crm_sales_details
+        SET @starttime = GETDATE();
+        PRINT 'Insertion des données dans crm_sales_details...';
         BULK INSERT bronze.crm_sales_details
         FROM 'D:\data-engeneering\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
         WITH (
@@ -94,8 +123,12 @@ BEGIN
             FIELDTERMINATOR = ',',
             TABLOCK
         );
+        SET @endtime = GETDATE();
+        PRINT 'Temps d exécution pour crm_sales_details BULK INSERT : ' + CAST(DATEDIFF(SECOND, @starttime, @endtime) AS VARCHAR(10)) + ' secondes';
 
-        PRINT 'Insertion des données dans erp_customer_az12 depuis le fichier CSV';
+        -- Insertion dans erp_customer_az12
+        SET @starttime = GETDATE();
+        PRINT 'Insertion des données dans erp_customer_az12...';
         BULK INSERT bronze.erp_customer_az12
         FROM 'D:\data-engeneering\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
         WITH (
@@ -103,8 +136,12 @@ BEGIN
             FIELDTERMINATOR = ',',
             TABLOCK
         );
+        SET @endtime = GETDATE();
+        PRINT 'Temps d exécution pour erp_customer_az12 BULK INSERT : ' + CAST(DATEDIFF(SECOND, @starttime, @endtime) AS VARCHAR(10)) + ' secondes';
 
-        PRINT 'Insertion des données dans erp_loc_a101 depuis le fichier CSV';
+        -- Insertion dans erp_loc_a101
+        SET @starttime = GETDATE();
+        PRINT 'Insertion des données dans erp_loc_a101...';
         BULK INSERT bronze.erp_loc_a101
         FROM 'D:\data-engeneering\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
         WITH (
@@ -112,8 +149,12 @@ BEGIN
             FIELDTERMINATOR = ',',
             TABLOCK
         );
+        SET @endtime = GETDATE();
+        PRINT 'Temps d exécution pour erp_loc_a101 BULK INSERT : ' + CAST(DATEDIFF(SECOND, @starttime, @endtime) AS VARCHAR(10)) + ' secondes';
 
-        PRINT 'Insertion des données dans erp_px_cat_g1v2 depuis le fichier CSV';
+        -- Insertion dans erp_px_cat_g1v2
+        SET @starttime = GETDATE();
+        PRINT 'Insertion des données dans erp_px_cat_g1v2...';
         BULK INSERT bronze.erp_px_cat_g1v2
         FROM 'D:\data-engeneering\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
         WITH (
@@ -121,20 +162,34 @@ BEGIN
             FIELDTERMINATOR = ',',
             TABLOCK
         );
+        SET @endtime = GETDATE();
+        PRINT 'Temps d exécution pour erp_px_cat_g1v2 BULK INSERT : ' + CAST(DATEDIFF(SECOND, @starttime, @endtime) AS VARCHAR(10)) + ' secondes';
+
+        -- ******************************************
+        -- Fin du script
+        -- ******************************************
 
         -- Validation de la transaction
         COMMIT TRANSACTION;
-        PRINT 'Données chargées avec succès';
+
+        -- Heure de fin du script
+        SET @total_endtime = GETDATE();
+        PRINT '======================================================================';
+        PRINT 'Script terminé - ' + CAST(@total_endtime AS VARCHAR(19));
+        PRINT 'Temps total d exécution : ' + CAST(DATEDIFF(SECOND, @total_starttime, @total_endtime) AS VARCHAR(10)) + ' secondes';
+        PRINT '======================================================================';
 
     END TRY
     BEGIN CATCH
-        -- En cas d'erreur, annuler la transaction et afficher l'erreur
+        -- Gestion des erreurs
         ROLLBACK TRANSACTION;
-        PRINT 'Erreur rencontrée: ' + ERROR_MESSAGE();
+
+        -- Détails sur l'erreur
+        PRINT '======================================================================';
+        PRINT 'Erreur rencontrée lors de l exécution :';
+        PRINT 'Code d erreur : ' + CAST(ERROR_NUMBER() AS VARCHAR(10));
+        PRINT 'Message d erreur : ' + ERROR_MESSAGE();
+        PRINT 'Procédure ou ligne où l erreur s est produite : ' + CAST(ERROR_LINE() AS VARCHAR(10));
+        PRINT '======================================================================';
     END CATCH
 END;
-
--- ******************************************
--- Fin du script
--- ******************************************
-
